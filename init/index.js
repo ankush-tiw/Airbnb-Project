20cbb003c3970284e4ec7438463e22cc616b8b32
Ankush Tiwari <tankush860@gamil.com>
2024-12-02 00:59:51 +0530

    Update

--- init/index.js
+++ init/index.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://tankush860:08WEuAtnb23XvkN9@cluster0.suyo3.mongodb.net/wanderlust";
+
 
 main()
   .then(() => {
@@ -18,7 +20,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "65309691ed551fd3edb77525"}));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "674cb7bbba14d5ebf5ab0833"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
